2934aef105d91928d8a9c8495e05c25e33850a3f
Imad Mohamad <yehya.mh@live.com>
2024-04-20 14:49:26 +0200

    Modified lower case in Exercise 1

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,7 +1,7 @@
 letter=input("Enter a String :").strip()
 vowel="a","e","i","u","y"
 for x in letter:
-    if x in vowel:
+    if x.lower() in vowel:
         print(f"{x} is a vowel.")
     elif x.strip():
         print(f"{x} is not a vowel.")
