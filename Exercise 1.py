923c0b616c2b6f462498d578546a58b61ced679e
Imad Mohamad <yehya.mh@live.com>
2024-04-20 14:47:12 +0200

    Modified 'if' in Exercise 1

--- Exercise 1.py
+++ Exercise 1.py
@@ -3,7 +3,9 @@ vowel="a","e","i","u","y"
 for x in letter:
     if x in vowel:
         print(f"{x} is a vowel.")
-    else:
+    elif x.strip():
         print(f"{x} is not a vowel.")
+    else:
+        print(f"{x} is empty.")
 if not letter:
     print("String is empty.")
