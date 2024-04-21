7089f82bd54d33b1f206b27cda8a1fa8821498ae
Imad Mohamad <yehya.mh@live.com>
2024-04-21 04:39:59 +0200

    Update Msg in ex 7

--- Exercise 7.py
+++ Exercise 7.py
@@ -4,7 +4,6 @@ def _GetNewQuwstion():
     x=random.randint(1, 10)
     y=random.randint(1, 10)
     operator=random.choice(["*", "+","-"])
-    result = False
     retresult = int(input(f"What is {x} {operator} {y}?"))
     
     if operator == "*":
@@ -14,20 +13,19 @@ def _GetNewQuwstion():
     else:
         result = retresult == x - y
         
-    if result:
-        print("That is correct!")
-    else:
-        print("That is false, you failed the test.")
     return result
-
 
 try:
     x=0
     while x < 3:
         if not _GetNewQuwstion():
+            print("That is false, you failed the test.")
             break
+        elif x < 2:
+            print("That is correct!")
+        else:
+            print("Correct! You passed the test!")
         x+=1
-        if x == 3:
-            print("All is correct! You passed the test!")
+        
 except ValueError:
     print("That's not a Number! \nThat is false, you failed the test.")
